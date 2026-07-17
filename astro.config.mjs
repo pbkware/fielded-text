b96e23f102dd26ca73c084e96a1426107d3eb45c
Paul Klink <paul@klink.au>
2026-07-17 13:20:17 +1000

    Update to Astro 7.0.9

--- astro.config.mjs
+++ astro.config.mjs
@@ -33,16 +33,7 @@ export default defineConfig({
     '/delphi-library/examples/basicwrite.html': '/delphi-library/examples/delphi-basic-writing-of-csv-file/',
   },
 
-  // Set the vite scss api property below if the following warning is displayed:
-  // Deprecation Warning: The legacy JS API is deprecated and will be removed in Dart Sass 2.0.0.
   vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          api: 'modern-compiler' // or "modern"
-        }
-      }
-    },
     assetsInclude: ['**/*.ftm'],
   }
 });
